27d3ef4e686ed4bd5d889a736ca8fcb51653a3a4
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-05-10 07:57:15 +0200

    apply formatting

--- MTK/modelingtoolkit.jl
+++ MTK/modelingtoolkit.jl
@@ -5,37 +5,35 @@ using ModelingToolkit, OrdinaryDiffEq, Plots
 @parameters t
 Dₜ = Differential(t)
 
-@parameters u(t) [input=true]  # Indicate that this is a controlled input
-@parameters y(t) [output=true] # Indicate that this is a measured output
+@parameters u(t) [input = true]  # Indicate that this is a controlled input
+@parameters y(t) [output = true] # Indicate that this is a measured output
 
 
 ## Build a component library ===================================================
 
 function Mass(; name, m = 1.0, p = 0, v = 0)
-    ps = @parameters m=m
-    sts = @variables pos(t)=p vel(t)=v
+    ps = @parameters m = m
+    sts = @variables pos(t) = p vel(t) = v
     eqs = Dₜ(pos) ~ vel
     ODESystem(eqs, t, [pos, vel], ps; name)
 end
 
-function Spring(; name, k = 1e4)
-    ps = @parameters k=k
-    @variables x(t)=0 # Spring deflection
+function MySpring(; name, k = 1e4)
+    ps = @parameters k = k
+    @variables x(t) = 0 # Spring deflection
     ODESystem(Equation[], t, [x], ps; name)
 end
 
-function Damper(; name, c = 10)
-    ps = @parameters c=c
-    @variables vel(t)=0
+function MyDamper(; name, c = 10)
+    ps = @parameters c = c
+    @variables vel(t) = 0
     ODESystem(Equation[], t, [vel], ps; name)
 end
 
-function SpringDamper(; name, k=false, c=false)
-    spring = Spring(; name=:spring, k)
-    damper = Damper(; name=:damper, c)
-    compose(
-        ODESystem(Equation[], t; name),
-        spring, damper)
+function SpringDamper(; name, k = false, c = false)
+    spring = MySpring(; name = :spring, k)
+    damper = MyDamper(; name = :damper, c)
+    compose(ODESystem(Equation[], t; name), spring, damper)
 end
 
 
@@ -51,17 +49,17 @@ m2 = 1
 k = 1000 # Spring stiffness
 c = 10   # Damping coefficient
 
-@named mass1 = Mass(; m=m1)
-@named mass2 = Mass(; m=m2)
+@named mass1 = Mass(; m = m1)
+@named mass2 = Mass(; m = m2)
 @named sd = SpringDamper(; k, c)
 
-function Model(u, d=0)
+function Model(u, d = 0)
     eqs = [
         connect_sd(sd, mass1, mass2)
-        Dₜ(mass1.vel) ~ ( sd_force(sd) + u) / mass1.m
+        Dₜ(mass1.vel) ~ (sd_force(sd) + u) / mass1.m
         Dₜ(mass2.vel) ~ (-sd_force(sd) + d) / mass2.m
     ]
-    @named _model = ODESystem(eqs, t; observed=[y~mass2.pos])
+    @named _model = ODESystem(eqs, t; observed = [y ~ mass2.pos])
     @named model = compose(_model, mass1, mass2, sd)
 end
 
@@ -69,7 +67,7 @@ end
 
 model = Model(sin(30t))
 sys = structural_simplify(model)
-prob = ODEProblem(sys, Pair[], (0., 1.))
+prob = ODEProblem(sys, Pair[], (0.0, 1.0))
 sol = solve(prob, Rodas5())
 plot(sol)
 
@@ -77,20 +75,26 @@ plot(sol)
 ## Close the loop ==============================================================
 
 using ModelingToolkitStandardLibrary.Blocks: LimPID, FirstOrder
-@variables u(t)=0 [input=true]
+@variables u(t) = 0 [input = true]
 model = Model(u)
-@named pid = LimPID(k=400, Ti=1, Td=1)
-@named filt = FirstOrder(T=0.1)
+@named pid = LimPID(k = 400, Ti = 1, Td = 1)
+@named filt = FirstOrder(T = 0.1)
 connections = [
-    filt.u  ~ t >= 1 # a reference step at t = 1
-    pid.reference.u ~ filt.y
+    filt.u            ~ t >= 1 # a reference step at t = 1
+    pid.reference.u   ~ filt.y
     pid.measurement.u ~ model.mass1.pos
     pid.ctr_output.u  ~ model.u
 ]
-closed_loop = structural_simplify(ODESystem(connections, t, systems=[model, pid, filt], name=:closed_loop))
-prob = ODEProblem(closed_loop, Pair[], (0., 4.))
+closed_loop = structural_simplify(
+    ODESystem(connections, t, systems = [model, pid, filt], name = :closed_loop),
+)
+prob = ODEProblem(closed_loop, Pair[], (0.0, 4.0))
 sol = solve(prob, Rodas5())#, dtmin=1e-15, force_dtmin=true);
-plot(plot(sol, vars=[filt.y, model.mass1.pos, model.mass2.pos]), plot(sol, vars=[pid.ctr_output.u], title="Control signal"), legend=:bottomright)
+plot(
+    plot(sol, vars = [filt.y, model.mass1.pos, model.mass2.pos]),
+    plot(sol, vars = [pid.ctr_output.u], title = "Control signal"),
+    legend = :bottomright,
+)
 
 
 
@@ -100,28 +104,28 @@ plot(plot(sol, vars=[filt.y, model.mass1.pos, model.mass2.pos]), plot(sol, vars=
 ## Using the standard library ==================================================
 using ModelingToolkitStandardLibrary.Mechanical.Rotational
 
-@named inertia1 = Inertia(; J=m1)
-@named inertia2 = Inertia(; J=m2)
+@named inertia1 = Inertia(; J = m1)
+@named inertia2 = Inertia(; J = m2)
 
-@named spring = Rotational.Spring(; c=k)
-@named damper = Rotational.Damper(; d=c)
+@named spring = Spring(; c = k)
+@named damper = Damper(; d = c)
 
 @named torque = Torque()
 
 
-function StdlibModel(u, d=0)
+function StdlibModel(u, d = 0)
     eqs = [
         torque.tau.u ~ u
         connect(torque.flange, inertia1.flange_a)
         connect(inertia1.flange_b, spring.flange_a, damper.flange_a)
         connect(inertia2.flange_a, spring.flange_b, damper.flange_b)
     ]
-    @named model = ODESystem(eqs, t; systems=[torque, inertia1, inertia2, spring, damper])
+    @named model = ODESystem(eqs, t; systems = [torque, inertia1, inertia2, spring, damper])
 end
 
 model = StdlibModel(sin(30t))
 sys = structural_simplify(model)
-prob = ODEProblem(sys, Pair[], (0., 1.))
+prob = ODEProblem(sys, Pair[], (0.0, 1.0))
 sol = solve(prob, Rodas5())
 plot(sol)
 
@@ -129,10 +133,11 @@ plot(sol)
 
 using ModelingToolkitStandardLibrary.Blocks: LimPID, FirstOrder
 using ModelingToolkitStandardLibrary.Mechanical.Rotational: AngleSensor
-@variables u(t)=0 [input=true]
+
+@variables u(t) = 0 [input = true]
 model = StdlibModel(u)
-@named pid = LimPID(k=400, Ti=1, Td=1)
-@named filt = FirstOrder(T=0.1)
+@named pid = LimPID(k = 400, Ti = 0.5, Td = 1, u_max=350)
+@named filt = FirstOrder(T = 0.1)
 @named sensor = AngleSensor()
 
 connections = [
@@ -142,8 +147,14 @@ connections = [
     connect(model.inertia1.flange_b, sensor.flange)
     connect(pid.measurement, sensor.phi)
 ]
-closed_loop = structural_simplify(ODESystem(connections, t, systems=[model, pid, filt, sensor], name=:closed_loop))
-prob = ODEProblem(closed_loop, Pair[], (0., 4.))
+closed_loop = structural_simplify(
+    ODESystem(connections, t, systems = [model, pid, filt, sensor], name = :closed_loop),
+)
+prob = ODEProblem(closed_loop, Pair[], (0.0, 4.0))
 sol = solve(prob, Rodas5())#, dtmin=1e-15, force_dtmin=true);
-plot(plot(sol, vars=[filt.y, model.inertia1.phi, model.inertia2.phi]), plot(sol, vars=[pid.ctr_output.u], title="Control signal"), legend=:bottomright)
+plot(
+    plot(sol, vars = [filt.y, model.inertia1.phi, model.inertia2.phi]),
+    plot(sol, vars = [pid.ctr_output.u], title = "Control signal"),
+    legend = :bottomright,
+)
 
